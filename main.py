5925b076d04b74fd0033e35f06a7df4621d863c0
rustleupchef <rustle.up.chef@gmail.com>
2024-11-28 01:36:48 -0600

    HashMap

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ with open('KEY.txt', 'r') as file:
     file.close()
     pass
 genai.configure(api_key=key)
-
-model = genai.GenerativeModel('gemini-1.5-flash')
-
-print(model.generate_content(contents="Howdy").text)
+with open('instructions.txt', 'r') as file:
+    instructions = file.read()
+    file.close()
+    pass
+model = genai.GenerativeModel('gemini-1.5-flash', system_instruction=instructions)
+for i in range(int(input("Enter how much data you want - "))):
+    text = model.generate_content(contents="Generate 100 Sample pieces of data").text
+    with open('DataSet/Set.csv', 'a') as file:
+        file.write(text)
+        file.close()
+        pass
--- src/App.java
+++ src/App.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        HashMap<String, Integer> words = new HashMap<String, Integer>();
         Scanner scanner = new Scanner(new File("DataSet/Set.csv"));
-        String text = "";
         while (scanner.hasNextLine()) {
-            text += scanner.nextLine() + '\n';
+            String text = scanner.nextLine();
+            String[] samples = text.toLowerCase().split(",");
+            for (String word : samples[0].split(" ")) {
+                System.out.println(samples[1]);
+                int coeffcient = (samples[1].equals("positive")) ? 1 : -1;
+                words.put(word, (words.containsKey(word) ? words.get(word) + coeffcient : 1 * coeffcient));
+            }
         }
-        System.out.println(text);
-        System.out.println(text.length() - 2);
-        text = text.substring(0, text.length());
-        FileWriter write = new FileWriter(new File("DataSet/Set.csv"));
-        write.write(text);
         scanner.close();
+        System.out.println(words);
     }
 }
